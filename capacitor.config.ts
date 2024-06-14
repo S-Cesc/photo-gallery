883f3bae6c2a739772c0f4be7fce3a61df0602c4
S-Cesc <cesc.sasal@gmail.com>
2024-06-14 14:09:43 +0200

    appId: 'io.ionic.starter.cesc' (channel development)

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,16 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.starter.cesc',
   appName: 'photo-gallery',
   webDir: 'www',
   plugins: {
     LiveUpdates: {
       channel: 'development',
+      appId: 'io.ionic.starter.cesc',
+      //channel: 'Production',
+      autoUpdateMethod: 'background',
+      maxVersions: 2
     }
   }
 };
