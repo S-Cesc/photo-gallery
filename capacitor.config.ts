f3a642eb7248a677129c6bb17f5f47039776ce7d
S-Cesc <cesc.sasal@gmail.com>
2024-06-14 13:12:19 +0200

    channel development

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'photo-gallery',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    LiveUpdates: {
+      channel: 'development',
+    }
+  }
 };
 
 export default config;
